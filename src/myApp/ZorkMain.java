338381f7a14ac5fd830e013025399fb7f9b0dc21
unknown <elliott.gel@gmail.com>
2022-12-14 22:10:20 +0800

    added user-input validation || added user-input eventhandler

--- src/myApp/ZorkMain.java
+++ src/myApp/ZorkMain.java
@@ -13,96 +13,106 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 import javax.management.Descriptor;
 
+import org.ietf.jgss.Oid;
+
 public class ZorkMain {
 
     public static List<Location> locations = new LinkedList<>();
-    public static String currentLocation = "";
+    public static String playerLocation = "";
+    public static Location currentLocation = null;
+    public static HashMap<String, String> locationDirections = null;
+    public static String input = "";
 
     public static void main(String[] args) {
         
         String fname = "location.txt";
 
-        try {
-        BufferedReader br = Files.newBufferedReader(Paths.get(fname));
-        
-        String contents = "";
-        String room = null;
-        String name = null;
-        String description = null;
-        HashMap<String, String> direction = new HashMap<>();
+        loadData(fname);
 
-        while((contents = br.readLine()) != null) {
-            
-            // if(contents.isEmpty()) {
-            //     continue;
-            // }
+        // Game Starting Text
+        currentLocation = getLocation(playerLocation);
+        locationDirections = currentLocation.getDirection();
 
-            String words[] = contents.split(":");
-            
-            switch (words[0]) {
-                case "":
-                    Location location = new Location(room, name, description, direction);
-                    locations.add(location);
-                    room = null; name = null; description = null; direction =  new HashMap<>();   
-                    break;
-                case "room":
-                    room = words[1];
-                    break;
-                case "name":
-                    name = words[1];
-                    break;
-                case "description":
-                    description = words[1].replaceAll("<break>", "\n");
-                    break;
-                case "direction":
-                    String[] word = words[1].trim().split(" ");
-                    //System.out.println(word[0] + "||" +  word[1]);
-                    direction.put(word[0], word[1]);
-                    break;
-                case "start":
-                    currentLocation = words[1];
-                    break;
-                default:
-            }
-        } 
-        //System.out.println(locations.get(0).getRoom());
-        } catch(IOException e) {}
-
-        // finished loading data
-        // starting console script
-
+        printHelper(currentLocation.getName(), currentLocation.getDescription());
         // print current location (room - computer // name - printed)
-        Location currentLocationObject = getLocation(currentLocation);
-        System.out.println(currentLocationObject.getName());
-        // print current description
-        System.out.println(currentLocationObject.getDescription());
-        System.out.println("");
+        
+        try(Scanner scanner = new Scanner(System.in)) {;
+            while(true) {
+                
+                input = scanner.nextLine();
+                String splitInput[] = input.split(" ");
 
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            String input = scanner.nextLine();
-            if (!(currentLocationObject.getDirection().keySet().contains(input.toLowerCase()))) {
-                System.out.printf("Cannot go %s\n", input);
-                continue;
-            }
-            String nextRoom = currentLocationObject.getDirection().get(input.trim().toLowerCase());
-            System.out.println(nextRoom);
-            
-            Location nextLocationObject = getLocation(nextRoom);
-            System.out.println(nextLocationObject.getName());
-            System.out.println(nextLocationObject.getDescription());
-            // System.out.println("");
-            // System.out.println("okaygeee");
-            // System.out.println(currentLocation);
-        // System.out.println(input);
+                // User hits enter, or types in spaces --> ignore
+                if (input.isBlank()) continue;
 
-        // >> wait for user <--- scanner
-        // read user input <--- scanner
-        // do things based on user input <--- switch <--??
+                switch(splitInput.length) {
+                    case 1:
+                        oneWordEvents(splitInput[0]);
+                        break;
+                    case 2:
+                        twoWordEvents(splitInput[0], splitInput[1]);
+                        break;
+                    default:
+                        printErrorHelper(input);
+                        break;
+                }
+            } 
+        } catch (Exception e) {e.getMessage();}
+    }
+
+    private static void loadData(String fname) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fname));) {
+            /* Black cat, white cat,
+             * Can catch mouse cat is good cat
+             * Use the BufferedReader you're most comfortabled with
+             * How you process the string from the .readLine method is most important
+             * Can work can already, dont think too hard */
+
+            String contents = "";
+            String room = null;
+            String name = null;
+            String description = null;
+            HashMap<String, String> direction = new HashMap<>();
+    
+            while((contents = br.readLine()) != null) {
+    
+                String words[] = contents.split(":");
+                
+                switch (words[0]) {
+                    case "":
+                        // An empty line in file signifies the end of a location block
+                        // Create a location instance with values from previous lines
+                        Location location = new Location(room, name, description, direction);
+                        // Add location instance to list
+                        locations.add(location);
+                        // Reset variable used to default value
+                        room = null; name = null; description = null; direction =  new HashMap<>();   
+                        break;
+                    case "room":
+                        room = words[1].trim();
+                        break;
+                    case "name":
+                        name = words[1].trim();
+                        break;
+                    case "description":
+                        description = words[1].trim().replaceAll("<break>", "\n");
+                        break;
+                    case "direction":
+                        String[] word = words[1].trim().split(" ");
+                        direction.put(word[0], word[1]);
+                        break;
+                    case "start":
+                        playerLocation = words[1].trim();
+                        break;
+                    default:
+                }
+            } 
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public static Location getLocation(String room) {
+    private static Location getLocation(String room) {
         Location loc = null;
         for (Location l : locations) { 
             if(l.getRoom().equals(room)) {
@@ -111,5 +121,66 @@ public class ZorkMain {
             }
         }
         return loc;
+    }
+
+    private static void printHelper(String location, String description) {
+        System.out.println(location);
+        System.out.println(description);
+        System.out.println("");
+    }
+
+    private static void printErrorHelper(String input) {
+        System.out.printf("I cannot understand the command: %s\n\n", input);
+    }
+
+    private static void oneWordEvents(String w1) {
+
+        if(w1.trim().equalsIgnoreCase("quit")){
+            System.out.println("Goodbye");
+            return;
+        }
+
+        if(w1.trim().equalsIgnoreCase("peek")){
+            printHelper(currentLocation.getName(), currentLocation.getDescription());
+            return;
+        }
+
+        if (!locationDirections.keySet().contains(w1.trim().toLowerCase())) {
+            printErrorHelper(input);
+            return;
+        }
+
+        if(locationDirections.keySet().contains(w1.trim().toLowerCase())) {
+            String nextRoom = locationDirections.get(w1.trim().toLowerCase());
+            Location nextLocation = getLocation(nextRoom);
+            printHelper(nextLocation.getName(), nextLocation.getDescription());
+            return;
+        }
+        System.out.println("If you see this, code is broken");
+        return;
+    }
+
+    private static void twoWordEvents(String w1, String w2) {
+        
+        // First word is not "go"
+        if (!w1.trim().equalsIgnoreCase("go")) {
+            printErrorHelper(input);
+            return;
+        }
+
+        // First word is "go"" --> check second word is valid
+        if (w1.trim().equalsIgnoreCase("go")) {
+            // second word is valid --> set variables
+            if (locationDirections.keySet().contains(w2.toLowerCase())) {
+                playerLocation = locationDirections.get(w2);
+                currentLocation = getLocation(playerLocation);
+                locationDirections = currentLocation.getDirection();
+                printHelper(currentLocation.getName(), currentLocation.getDescription());
+                return;
+            } else { 
+                printErrorHelper(input);
+                return;
+            }
+        }
     }
 }
